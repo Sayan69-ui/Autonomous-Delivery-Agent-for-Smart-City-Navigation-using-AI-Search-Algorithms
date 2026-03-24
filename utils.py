ef7501f4e280bfab346f976b885e5e0d98335db7
Sayan69-ui <bhowmiksayan0506@gmail.com>
2026-03-24 21:28:51 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def reconstruct_path(came_from, start, goal):
-    cur = goal
-    path = []
-    while cur != start:
-        path.append(cur)
-        cur = came_from[cur]
-    path.append(start)
-    path.reverse()
-    return path
